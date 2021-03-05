c4e294fab508e372468e5814493e12f92c4492be
Raphaël BEVENOT <raphael@bevenot.fr>
2021-03-05 13:30:56 +0100

    Adding registration in database

--- code/app.py
+++ code/app.py
@@ -3,6 +3,8 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
+from user import UserRegister
+
 
 app = Flask(__name__)
 app.secret_key = 'raphael'
@@ -61,5 +63,6 @@ class ItemList(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 app.run(debug=True)
