65da34c99953d24b0d1921d2ea5cf761c1fbcef9
Raphaël BEVENOT <raphael@bevenot.fr>
2021-03-08 00:29:12 +0100

    Adding delete method

--- code/item.py
+++ code/item.py
@@ -12,33 +12,66 @@ class Item(Resource):
                         help="this field cannot be left blank"
                         )
 
-    @jwt_required()
-    def get(self, name):
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
         query = "SELECT * FROM items WHERE name=?"
+        # Execute need a tuple in parameters
         result = cursor.execute(query, (name,))
         row = result.fetchone()
         connection.close()
-
         if row:
             return {"item": {"name": row[0], "price": row[1]}}
+        return None
+
+    @jwt_required()
+    def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
         return {"message": "Item not found"}, 400
 
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
+        if self.find_by_name(name):
             return {"message": "An item with name '{}' already exists.".format(name)}, 400
 
         data = Item.parser.parse_args()
 
         item = {'name': name, 'price': data['price']}
-        items.append(item)
+
+        try:
+            cls.insert(item)
+        except:
+            return {"message": "An error occurred inserting the item"}, 500
+            # internal server error
         return item, 201
 
+    @classmethod
+    def insert(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
+        if not self.find_by_name(name):
+            return {"message": "There is no existing item named '{}'.".format(name)}, 400
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name = ?"
+        cursor.execute(query, (name,))
+
+        connection.commit()
+        connection.close()
+
         return {"message": "Item deleted"}
 
     def put(self, name):
