dacfd686f2b62fd93a57241e3484b9c0c5ff93b5
Raphaël BEVENOT <raphael@bevenot.fr>
2021-03-08 00:52:24 +0100

    Adding update method

--- code/item.py
+++ code/item.py
@@ -42,11 +42,22 @@ class Item(Resource):
         item = {'name': name, 'price': data['price']}
 
         try:
-            cls.insert(item)
+            self.insert(item)
         except:
             return {"message": "An error occurred inserting the item"}, 500
             # internal server error
         return item, 201
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name = ?"
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close()
 
     @classmethod
     def insert(cls, item):
@@ -76,14 +87,22 @@ class Item(Resource):
 
     def put(self, name):
         data = Item.parser.parse_args()
+        item = self.find_by_name(name)
 
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        updated_item = {'name': name, 'price': data['price']}
+
         if item is None:
-            item = {"name": name, "price": data["price"]}
-            items.append(item)
+            try:
+                self.insert(updated_item)
+            except:
+                return {"message": "An error occurred inserting the item"}, 500
         else:
-            item.update(data)
-        return item
+            try:
+                self.update(updated_item)
+            except:
+                {"message": "An error occurred updating the item"}, 500
+
+        return updated_item, 201
 
 
 class ItemList(Resource):
